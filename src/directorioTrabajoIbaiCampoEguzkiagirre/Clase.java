3dd66d29c64199b49b6a462bc037a7099e21fc8a
unknown <DAM128@iesataulfo.local>
2024-06-11 16:46:12 +0200

    Tercera fase proyecto (Clase y Principal creadas y comentadas)

--- src/directorioTrabajoIbaiCampoEguzkiagirre/Clase.java
+++ src/directorioTrabajoIbaiCampoEguzkiagirre/Clase.java
@@ -1,6 +1,9 @@
 package directorioTrabajoIbaiCampoEguzkiagirre;
 
 public class Clase {
+	
+	// Tercera fase
+	
 	public Clase() {
 		System.out.println("Soy un objeto de la clase y acabo de ser creado");
 	}
